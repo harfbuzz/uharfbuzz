f8837fdccffa3c517baeb30c45a3ffc59157576f
Cosimo Lupo <cosimo@anthrotype.com>
2018-03-18 19:10:54 +0000

    setup.py: use scikit-build instead of setuptools; add metadata

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Build import cythonize
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from skbuild import setup
+
 
 setup(
-    ext_modules = cythonize([
-        Extension("harfbuzz", ["src/harfbuzz.pyx"], libraries=["harfbuzz"])])
+    name="uharfbuzz",
+    version="0.1.0.dev0",
+    description="Streamlined Cython bindings for the harfbuzz shaping engine",
+    author="Adrien Tétar",
+    author_email="adri-from-59@hotmail.fr",
+    url="https://github.com/trufont/uharfbuzz",
+    license="Apache License 2.0",
+    package_dir={"": "src"},
+    packages=["uharfbuzz"],
+    zip_safe=False,
 )
