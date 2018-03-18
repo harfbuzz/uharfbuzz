0d16631ffc5f7862b6789a9987b4c6a6a4fc87ea
Cosimo Lupo <cosimo@anthrotype.com>
2018-03-18 19:00:46 +0000

    _harfbuzz.pyx: cimport charfbuzz; remove c.* prefix

--- src/uharfbuzz/_harfbuzz.pyx
+++ src/uharfbuzz/_harfbuzz.pyx
@@ -1,54 +1,54 @@
 from functools import partial
-cimport harfbuzz as c
 # currently we malloc but dont free..
+from charfbuzz cimport *
 from libc.stdlib cimport free, malloc
 from typing import Callable, List, Tuple
 
 
 cdef class Buffer:
-    cdef c.hb_buffer_t* _hb_buffer
+    cdef hb_buffer_t* _hb_buffer
 
     def __cinit__(self):
         self._hb_buffer = NULL
 
     def __dealloc__(self):
         if self._hb_buffer is not NULL:
-            c.hb_buffer_destroy(self._hb_buffer)
+            hb_buffer_destroy(self._hb_buffer)
 
     @classmethod
     def create(cls):
         cdef Buffer inst = cls()
-        inst._hb_buffer = c.hb_buffer_create()
+        inst._hb_buffer = hb_buffer_create()
         return inst
 
     @property
     def direction(self) -> str:
-        return c.hb_direction_to_string(
-            c.hb_buffer_get_direction(self._hb_buffer))
+        return hb_direction_to_string(
+            hb_buffer_get_direction(self._hb_buffer))
 
     @direction.setter
     def direction(self, value: str):
         packed = value.encode()
         cdef char* cstr = packed
-        c.hb_buffer_set_direction(
-            self._hb_buffer, c.hb_direction_from_string(cstr, -1))
+        hb_buffer_set_direction(
+            self._hb_buffer, hb_direction_from_string(cstr, -1))
 
     @property
     def language(self) -> str:
-        return c.hb_language_to_string(
-            c.hb_buffer_get_language(self._hb_buffer))
+        return hb_language_to_string(
+            hb_buffer_get_language(self._hb_buffer))
 
     @language.setter
     def language(self, value: str):
         packed = value.encode()
         cdef char* cstr = packed
-        c.hb_buffer_set_language(
-            self._hb_buffer, c.hb_language_from_string(cstr, -1))
+        hb_buffer_set_language(
+            self._hb_buffer, hb_language_from_string(cstr, -1))
 
     @property
     def script(self) -> str:
         cdef char cstr[5]
-        c.hb_tag_to_string(c.hb_buffer_get_script(self._hb_buffer), cstr)
+        hb_tag_to_string(hb_buffer_get_script(self._hb_buffer), cstr)
         cdef bytes packed = cstr
         return packed.decode()
 
@@ -58,8 +58,8 @@ cdef class Buffer:
         cdef char* cstr = packed
         # all the *_from_string calls should probably be checked and throw an
         # exception if NULL
-        c.hb_buffer_set_script(
-            self._hb_buffer, c.hb_script_from_string(cstr, -1))
+        hb_buffer_set_script(
+            self._hb_buffer, hb_script_from_string(cstr, -1))
 
     def add_codepoints(self, codepoints: List[int],
                        item_offset: int = None, item_length: int = None) -> None:
@@ -68,11 +68,11 @@ cdef class Buffer:
             item_offset = 0
         if item_length is None:
             item_length = size
-        cdef c.hb_codepoint_t* hb_codepoints = <c.hb_codepoint_t*>malloc(
-                size * sizeof(c.hb_codepoint_t))
+        cdef hb_codepoint_t* hb_codepoints = <hb_codepoint_t*>malloc(
+                size * sizeof(hb_codepoint_t))
         for i in range(size):
             hb_codepoints[i] = codepoints[i]
-        c.hb_buffer_add_codepoints(
+        hb_buffer_add_codepoints(
             self._hb_buffer, hb_codepoints, size, item_offset, item_length)
 
     def add_str(self, text: str,
@@ -84,7 +84,7 @@ cdef class Buffer:
         if item_length is None:
             item_length = size
         cdef char* cstr = packed
-        c.hb_buffer_add_utf8(
+        hb_buffer_add_utf8(
             self._hb_buffer, cstr, size, item_offset, item_length)
 
     def guess_segment_properties(self) -> None:
@@ -95,20 +95,20 @@ cdef hb_user_data_key_t k
 
 cdef hb_blob_t* _reference_table_func(
         hb_face_t* face, hb_tag_t tag, void* user_data):
-    cdef Face py_face = <object>(c.hb_face_get_user_data(face, &k))
+    cdef Face py_face = <object>(hb_face_get_user_data(face, &k))
     #
     cdef char cstr[5]
-    c.hb_tag_to_string(tag, cstr)
+    hb_tag_to_string(tag, cstr)
     cdef bytes packed = cstr
     #
     cdef bytes table = py_face._reference_table_func(
         py_face, packed.decode(), <object>user_data)
-    return c.hb_blob_create(
+    return hb_blob_create(
         table, len(table), HB_MEMORY_MODE_READONLY, NULL, NULL)
 
 
 cdef class Face:
-    cdef c.hb_face_t* _hb_face
+    cdef hb_face_t* _hb_face
     cdef object _reference_table_func
 
     def __cinit__(self):
@@ -116,14 +116,14 @@ cdef class Face:
 
     def __dealloc__(self):
         if self._hb_face is not NULL:
-            c.hb_face_destroy(self._hb_face)
+            hb_face_destroy(self._hb_face)
         self._func = None
 
     """ use bytes/bytearray, not Blob
     @classmethod
     def create(self, blob: Blob, index: int):
         cdef Face inst = cls()
-        inst._hb_face = c.hb_face_create(blob, index)
+        inst._hb_face = hb_face_create(blob, index)
         return inst
     """
 
@@ -136,23 +136,23 @@ cdef class Face:
                           ], bytes],
                           user_data: object):
         cdef Face inst = cls()
-        inst._hb_face = c.hb_face_create_for_tables(
+        inst._hb_face = hb_face_create_for_tables(
             _reference_table_func, <void*>user_data, NULL)
-        c.hb_face_set_user_data(inst._hb_face, &k, <void*>inst, NULL, 0)
+        hb_face_set_user_data(inst._hb_face, &k, <void*>inst, NULL, 0)
         inst._reference_table_func = func
         return inst
 
     @property
     def upem(self) -> int:
-        return c.hb_face_get_upem(self._hb_face)
+        return hb_face_get_upem(self._hb_face)
 
     @upem.setter
     def upem(self, value: int):
-        c.hb_face_set_upem(self._hb_face, value)
+        hb_face_set_upem(self._hb_face, value)
 
 
 cdef class Font:
-    cdef c.hb_font_t* _hb_font
+    cdef hb_font_t* _hb_font
     # GC bookkeeping
     cdef Face _face
     cdef FontFuncs _ffuncs
@@ -162,13 +162,13 @@ cdef class Font:
 
     def __dealloc__(self):
         if self._hb_font is not NULL:
-            c.hb_font_destroy(self._hb_font)
+            hb_font_destroy(self._hb_font)
         self._face = self._ffuncs = None
 
     @classmethod
     def create(cls, face: Face):
         cdef Font inst = cls()
-        inst._hb_font = c.hb_font_create(face._hb_face)
+        inst._hb_font = hb_font_create(face._hb_face)
         inst._face = face
         return inst
 
@@ -178,40 +178,40 @@ cdef class Font:
 
     @funcs.setter
     def funcs(self, ffuncs: FontFuncs):
-        c.hb_font_set_funcs(
+        hb_font_set_funcs(
             self._hb_font, ffuncs._hb_ffuncs, <void*>self, NULL)
         self._ffuncs = ffuncs
 
     @property
     def scale(self) -> Tuple[int, int]:
         cdef int x, y
-        c.hb_font_get_scale(self._hb_font, &x, &y)
+        hb_font_get_scale(self._hb_font, &x, &y)
         return (x, y)
 
     @scale.setter
     def scale(self, value: Tuple[int, int]):
         x, y = value
-        c.hb_font_set_scale(self._hb_font, x, y)
+        hb_font_set_scale(self._hb_font, x, y)
 
     def shape(self, buffer: Buffer, features: List[str] = None) -> None:
         cdef int size
-        cdef c.hb_feature_t* hb_features
+        cdef hb_feature_t* hb_features
         if features is None:
             size = 0
             hb_features = NULL
         else:
             size = len(features)
-            hb_features = <c.hb_feature_t*>malloc(
-                size * sizeof(c.hb_feature_t))
+            hb_features = <hb_feature_t*>malloc(
+                size * sizeof(hb_feature_t))
         cdef bytes packed
         cdef char* cstr
         cdef hb_feature_t feat
         for i in range(size):
             packed = features[i].encode()
             cstr = packed
-            c.hb_feature_from_string(packed, len(packed), &feat)
+            hb_feature_from_string(packed, len(packed), &feat)
             hb_features[i] = feat
-        c.hb_shape(self._hb_font, buffer._hb_buffer, hb_features, size)
+        hb_shape(self._hb_font, buffer._hb_buffer, hb_features, size)
 
 
 cdef hb_position_t _glyph_h_advance_func(hb_font_t* font, void* font_data,
@@ -244,7 +244,7 @@ cdef hb_bool_t _nominal_glyph_func(hb_font_t* font, void* font_data,
 
 
 cdef class FontFuncs:
-    cdef c.hb_font_funcs_t* _hb_ffuncs
+    cdef hb_font_funcs_t* _hb_ffuncs
     cdef object _glyph_h_advance_func
     cdef object _glyph_name_func
     cdef object _nominal_glyph_func
@@ -254,12 +254,12 @@ cdef class FontFuncs:
 
     def __dealloc__(self):
         if self._hb_ffuncs is not NULL:
-            c.hb_font_funcs_destroy(self._hb_ffuncs)
+            hb_font_funcs_destroy(self._hb_ffuncs)
 
     @classmethod
     def create(cls):
         cdef FontFuncs inst = cls()
-        inst._hb_ffuncs = c.hb_font_funcs_create()
+        inst._hb_ffuncs = hb_font_funcs_create()
         return inst
 
     def set_glyph_h_advance_func(self,
@@ -269,7 +269,7 @@ cdef class FontFuncs:
                                      object,  # user_data
                                  ], int],  # h_advance
                                  user_data: object) -> None:
-        c.hb_font_funcs_set_glyph_h_advance_func(
+        hb_font_funcs_set_glyph_h_advance_func(
             self._hb_ffuncs, _glyph_h_advance_func, <void*>user_data, NULL)
         self._glyph_h_advance_func = func
 
@@ -280,7 +280,7 @@ cdef class FontFuncs:
                                 object,  # user_data
                             ], str],  # name
                             user_data: object) -> None:
-        c.hb_font_funcs_set_glyph_name_func(
+        hb_font_funcs_set_glyph_name_func(
             self._hb_ffuncs, _glyph_name_func, <void*>user_data, NULL)
         self._glyph_name_func = func
 
@@ -291,7 +291,7 @@ cdef class FontFuncs:
                                    object,  # user_data
                                ], int],  # gid
                                user_data: object) -> None:
-        c.hb_font_funcs_set_nominal_glyph_func(
+        hb_font_funcs_set_nominal_glyph_func(
             self._hb_ffuncs, _nominal_glyph_func, <void*>user_data, NULL)
         self._nominal_glyph_func = func
 
