2c18cb017a9a3c7bd542cea88d1bc2c5360595e0
Cosimo Lupo <cosimo@anthrotype.com>
2018-12-12 18:27:03 +0000

    test calling add_str with ascii, latin1, ucs2 and ucs4
    once we add some test fonts we shall assert that the shaped results are as expected.
    for now these tests simply 'cover' the lines..

--- tests/test_uharfbuzz.py
+++ tests/test_uharfbuzz.py
@@ -8,3 +8,19 @@ class TestBuffer:
 
     def test_create(self):
         buf = hb.Buffer.create()
+
+    def test_add_str_ascii(self):
+        buf = hb.Buffer()
+        buf.add_str("abcde")
+
+    def test_add_str_latin1(self):
+        buf = hb.Buffer()
+        buf.add_str("abçde")
+
+    def test_add_str_ucs2(self):
+        buf = hb.Buffer()
+        buf.add_str("aбcde")
+
+    def test_add_str_ucs4(self):
+        buf = hb.Buffer()
+        buf.add_str("abcd💩e")
