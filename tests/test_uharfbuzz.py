ae4ebe288847d3fb5a8686894c2ec1600439abd7
Cosimo Lupo <cosimo@anthrotype.com>
2018-12-13 11:32:10 +0000

    Add subset of AdobeBlank.ttf and test cluster values before/after hb-shape

--- tests/test_uharfbuzz.py
+++ tests/test_uharfbuzz.py
@@ -1,26 +1,105 @@
 import uharfbuzz as hb
+from pathlib import Path
+import pytest
+
+
+TESTDATA = Path(__file__).parent / "data"
+ADOBE_BLANK_TTF = (TESTDATA / "AdobeBlank.subset.ttf").read_bytes()
+
+
+@pytest.fixture
+def blankfont():
+    """Return a subset of AdobeBlank.ttf containing the following glyphs/characters:
+    [
+        {gid=0, name=".notdef"},
+        {gid=1, name="a", code=0x61},
+        {gid=2, name="b", code=0x62},
+        {gid=3, name="c", code=0x63},
+        {gid=4, name="d", code=0x64},
+        {gid=5, name="e", code=0x65},
+        {gid=6, name="ccedilla", code=0x62},
+        {gid=7, name="uni0431", code=0x0431},  # CYRILLIC SMALL LETTER BE
+        {gid=8, name="u1F4A9", code=0x1F4A9},  # PILE OF POO
+    ]
+    """
+    face = hb.Face(ADOBE_BLANK_TTF)
+    font = hb.Font(face)
+    upem = face.upem
+    font.scale = (upem, upem)
+    hb.ot_font_set_funcs(font)
+    return font
 
 
 class TestBuffer:
-
     def test_init(self):
         buf = hb.Buffer()
 
     def test_create(self):
         buf = hb.Buffer.create()
 
-    def test_add_str_ascii(self):
+    @pytest.mark.parametrize(
+        "string, expected",
+        [
+            ("abcde", [(0x61, 0), (0x62, 1), (0x63, 2), (0x64, 3), (0x65, 4)]),
+            ("abçde", [(0x61, 0), (0x62, 1), (0xE7, 2), (0x64, 3), (0x65, 4)]),
+            ("aбcde", [(0x61, 0), (0x431, 1), (0x63, 2), (0x64, 3), (0x65, 4)]),
+            ("abc💩e", [(0x61, 0), (0x62, 1), (0x63, 2), (0x1F4A9, 3), (0x65, 4)]),
+        ],
+        ids=["ascii", "latin1", "ucs2", "ucs4"],
+    )
+    def test_add_str(self, string, expected):
         buf = hb.Buffer()
-        buf.add_str("abcde")
+        buf.add_str(string)
+        infos = [(g.codepoint, g.cluster) for g in buf.glyph_infos]
+        assert infos == expected
 
-    def test_add_str_latin1(self):
+    def test_add_utf8(self):
         buf = hb.Buffer()
-        buf.add_str("abçde")
+        buf.add_utf8("aбç💩e".encode("utf-8"))
+        infos = [(g.codepoint, g.cluster) for g in buf.glyph_infos]
+        assert infos == [(0x61, 0), (0x431, 1), (0xE7, 3), (0x1F4A9, 5), (0x65, 9)]
 
-    def test_add_str_ucs2(self):
+    def test_add_codepoints(self):
         buf = hb.Buffer()
-        buf.add_str("aбcde")
+        buf.add_codepoints([0x61, 0x431, 0xE7, 0x1F4A9, 0x65])
+        infos = [(g.codepoint, g.cluster) for g in buf.glyph_infos]
+        assert infos == [(0x61, 0), (0x431, 1), (0xE7, 2), (0x1F4A9, 3), (0x65, 4)]
 
-    def test_add_str_ucs4(self):
+    def test_guess_set_segment_properties(self):
         buf = hb.Buffer()
-        buf.add_str("abcd💩e")
+        buf.add_str("הארץ")
+
+        buf.guess_segment_properties()
+
+        assert buf.direction == "rtl"
+        assert buf.script == "Hebr"
+        assert buf.language == "en-us"
+
+        buf.direction = "ltr"
+        assert buf.direction == "ltr"
+
+        buf.script = "Latn"
+        assert buf.script == "Latn"
+
+        buf.language = "he-il"
+        assert buf.language == "he-il"
+
+
+class TestShape:
+    @pytest.mark.parametrize(
+        "string, expected",
+        [
+            ("abcde", [(1, 0), (2, 1), (3, 2), (4, 3), (5, 4)]),
+            ("abçde", [(1, 0), (2, 1), (6, 2), (4, 3), (5, 4)]),
+            ("aбcde", [(1, 0), (7, 1), (3, 2), (4, 3), (5, 4)]),
+            ("abc💩e", [(1, 0), (2, 1), (3, 2), (8, 3), (5, 4)]),
+        ],
+        ids=["ascii", "latin1", "ucs2", "ucs4"],
+    )
+    def test_gid_and_cluster_no_features(self, blankfont, string, expected):
+        buf = hb.Buffer()
+        buf.add_str(string)
+        buf.guess_segment_properties()
+        hb.shape(blankfont, buf)
+        infos = [(g.codepoint, g.cluster) for g in buf.glyph_infos]
+        assert infos == expected
