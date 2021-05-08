53098fd9d2d21b3289c07a107c640a4d8bf618a8
Just van Rossum <justvanrossum@gmail.com>
2021-05-08 07:12:35 +0200

    Add font.get_glyph_extents() wrapper for hb_font_get_glyph_extents() (#80)
    * add font.get_glyph_extents(gid)
    
    * add test for font.get_glyph_name()
    
    * test bad gid

--- tests/test_uharfbuzz.py
+++ tests/test_uharfbuzz.py
@@ -134,6 +134,20 @@ class TestBuffer:
         assert buf.cluster_level == 1
 
 
+class TestFont:
+    def test_get_glyph_extents(self, opensans):
+        # <TTGlyph name="A" xMin="0" yMin="0" xMax="1296" yMax="1468">
+        x, y, w, h = opensans.get_glyph_extents(1)
+        assert (0, 1468, 1296, -1468) == (x, y, w, h)
+        assert opensans.get_glyph_extents(1000) is None
+
+    def test_get_glyph_name(self, blankfont):
+        glyph_name = blankfont.get_glyph_name(1)
+        assert glyph_name == "a"
+        glyph_name = blankfont.get_glyph_name(1000)
+        assert glyph_name is None
+
+
 class TestShape:
     @pytest.mark.parametrize(
         "string, expected",
