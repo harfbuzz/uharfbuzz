7a78cce1bf9f62a777c2006d3fb5f969ae680886
Khaled Hosny <khaled@aliftype.com>
2026-06-02 01:03:26 +0300

    Strip uharfbuzz._harfbuzz prefix from rendered signatures

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,6 +19,25 @@ extensions = [
     "myst_parser",
 ]
 
+# Cython emits parameter annotations as string forward references (e.g.
+# `font: 'Font'`). Sphinx renders these as `~uharfbuzz._harfbuzz.Font` with
+# the `~` displayed literally. Strip the qualifier ourselves.
+import re as _re
+
+_QUALIFIED_TYPE_RE = _re.compile(r"~?uharfbuzz(?:\._harfbuzz)?\.")
+
+
+def _strip_module_prefix(app, what, name, obj, options, signature, return_annotation):
+    if signature:
+        signature = _QUALIFIED_TYPE_RE.sub("", signature)
+    if return_annotation:
+        return_annotation = _QUALIFIED_TYPE_RE.sub("", return_annotation)
+    return signature, return_annotation
+
+
+def setup(app):
+    app.connect("autodoc-process-signature", _strip_module_prefix)
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
